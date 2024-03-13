13e792c6ecc5631a4364fb7459f5cb01be29f685
Amit <amitbhuse07@gmail.com>
2024-03-13 16:13:27 +0530

    First Commit

---
+++ src/main/java/in/ineuron/service/CustomerServiceImpl.java
@@ -0,0 +1,39 @@
+package in.ineuron.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import in.ineuron.dao.ICustomerDAO;
+import in.ineuron.model.Customer;
+
+@Service
+public class CustomerServiceImpl implements ICustomerService {
+	
+	@Autowired
+	private ICustomerDAO repo;
+
+	@Override
+	public List<Customer> getCustomers() {
+		return (List<Customer>) repo.findAll();
+	}
+
+	@Override
+	public void saveCustomer(Customer customer) {
+		repo.save(customer);
+	}
+
+	@Override
+	public Customer getCustomer(Integer customerId) {
+		Optional<Customer> customer = repo.findById(customerId);
+		return customer.get();
+	}
+
+	@Override
+	public void deleteCustomer(Integer customerId) {
+		repo.deleteById(customerId);
+	}
+
+}
